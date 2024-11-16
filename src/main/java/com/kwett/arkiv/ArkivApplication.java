9711e9a12028dfe2f4e3e3ae13009b3a2b1458fa
Kwett <coudrin.clement@gmail.com>
2024-11-16 20:36:41 +0100

    Init

---
+++ src/main/java/com/kwett/arkiv/ArkivApplication.java
@@ -0,0 +1,13 @@
+package com.kwett.arkiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkivApplication.class, args);
+	}
+
+}
