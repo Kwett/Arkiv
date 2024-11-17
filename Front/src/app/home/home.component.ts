a027339396d6f667d42808350ba892aa5212a55f
Kwett <coudrin.clement@gmail.com>
2024-11-17 17:38:17 +0100

    add front interface for desktop

--- Front/src/app/home/home.component.ts
+++ Front/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FileInfo } from '../models/fileInfo.model';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  protected files: FileInfo[] = [
+    {name: 'INTRODUCTION - Robin DICKINSON.mp4', size: 72, date: '17/11/2024 - 11:00'},
+    {name: 'INTRODUCTION - Robin DICKINSON.mp4', size: 72, date: '17/11/2024 - 11:00'}
+  ];
 }
---
+++ Front/src/app/models/fileInfo.model.ts
@@ -0,0 +1,5 @@
+export interface FileInfo {
+    name: String,
+    size: Number,
+    date: String
+}
