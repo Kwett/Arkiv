f4919c042c156e5b1d9d58482cb0063b4d17ba26
Kwett <coudrin.clement@gmail.com>
2024-11-17 04:47:57 +0100

    Adds download feature

--- src/main/java/com/kwett/arkiv/service/RssService.java
+++ src/main/java/com/kwett/arkiv/service/RssService.java
@@ -1,5 +1,7 @@
 package com.kwett.arkiv.service;
 
+import com.github.kiulian.downloader.downloader.YoutubeProgressCallback;
+import com.github.kiulian.downloader.downloader.response.Response;
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
@@ -10,8 +12,15 @@ import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import com.github.kiulian.downloader.YoutubeDownloader;
+import com.github.kiulian.downloader.model.videos.VideoDetails;
+import com.github.kiulian.downloader.model.videos.VideoInfo;
+import com.github.kiulian.downloader.model.videos.formats.VideoWithAudioFormat;
+import com.github.kiulian.downloader.downloader.request.RequestVideoFileDownload;
+import com.github.kiulian.downloader.downloader.request.RequestVideoInfo;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.util.HashSet;
@@ -27,12 +36,15 @@ public class RssService {
     private final String rssUrl;
     private final Set<String> knownVideoIds = new HashSet<>();
     private static final Logger logger = LoggerFactory.getLogger(RssService.class);
+    private final String downloadFolder;
 
     public RssService(
             @Value("${video.ids.file}") String videoIdsFile,
-            @Value("${rss.url}") String rssUrl) {
+            @Value("${rss.url}") String rssUrl,
+            @Value("${download.folder}") String downloadFolder){
         this.videoIdsFile = videoIdsFile;
         this.rssUrl = rssUrl;
+        this.downloadFolder = downloadFolder;
 
         logger.info("RSS URL: {}", rssUrl);
         logger.info("Video IDs File: {}", videoIdsFile);
@@ -63,9 +75,11 @@ public class RssService {
 
                 if (!knownVideoIds.contains(videoId)) {
                     knownVideoIds.add(videoId);
-                    logger.info("Nouvelle vidéo détectée : Titre : {} | Lien : {}", entry.getTitle(), entry.getLink());
+                    logger.info("Nouvelle vidéo détectée : Titre : {} | Lien : {}", entry.getTitle(), videoId);
+                    downloadVideo(videoId);
                     saveKnownVideos();
                 }
+
             }
         } catch (IOException e) {
             logger.error("Erreur d'entrée/sortie lors du traitement du flux RSS : ", e);
@@ -86,6 +100,87 @@ public class RssService {
             logger.error("Erreur lors de la sauvegarde des vidéos connues : ", e);
         }
     }
+
+    public void downloadVideo(String videoUrl) {
+        try {
+            YoutubeDownloader downloader = new YoutubeDownloader();
+
+            String videoId;
+
+            if (videoUrl.contains("v=")) {
+                videoId = videoUrl.split("v=")[1];
+                if (videoId.contains("&")) {
+                    videoId = videoId.split("&")[0];
+                }
+            } else if (videoUrl.startsWith("yt:video:")) {
+                videoId = videoUrl.replace("yt:video:", "");
+            } else {
+                logger.error("URL vidéo invalide : {}", videoUrl);
+                return;
+            }
+
+            RequestVideoInfo requestInfo = new RequestVideoInfo(videoId);
+            Response<VideoInfo> responseInfo = downloader.getVideoInfo(requestInfo);
+            VideoInfo videoInfo = responseInfo.data();
+
+
+            VideoDetails details = videoInfo.details();
+
+            System.out.println("Titre : " + details.title());
+            System.out.println("Durée : " + details.lengthSeconds() + " secondes");
+
+            if (details.isLive()) {
+                logger.warn("La vidéo est un live stream et ne peut pas être téléchargée pour le moment.");
+                return;
+            }
+
+
+            VideoWithAudioFormat format = (VideoWithAudioFormat) videoInfo.bestVideoWithAudioFormat();
+
+            if (format == null) {
+                logger.error("Aucun format vidéo avec audio disponible pour : {}", details.title());
+                return;
+            }
+
+            File downloadsDir = new File(downloadFolder);
+            if (!downloadsDir.exists()) {
+                boolean created = downloadsDir.mkdirs();
+                if (!created) {
+                    logger.error("Impossible de créer le dossier : {}", downloadsDir.getAbsolutePath());
+                    throw new IOException("Erreur lors de la création du dossier " + downloadsDir.getAbsolutePath());
+                }
+            }
+
+            RequestVideoFileDownload requestDownload = new RequestVideoFileDownload(format)
+                    .saveTo(downloadsDir)
+                    .renameTo(details.title())
+                    .overwriteIfExists(true);
+
+            Response<File> responseDownload = downloader.downloadVideoFile(requestDownload);
+            File downloadedFile = responseDownload.data();
+            requestDownload.callback(new YoutubeProgressCallback<>() {
+                @Override
+                public void onDownloading(int progress) {
+                    logger.info("Téléchargement en cours : {}% pour {}", progress, details.title());
+                }
+
+                @Override
+                public void onFinished(File file) {
+                    logger.info("Téléchargement terminé : {}", file.getAbsolutePath());
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    logger.error("Erreur lors du téléchargement : ", throwable);
+                }
+            });
+
+            System.out.println("Téléchargement terminé : " + downloadedFile.getAbsolutePath());
+        } catch (Exception e) {
+            logger.error("erreur :", e);
+        }
+    }
 }
+
 
 
