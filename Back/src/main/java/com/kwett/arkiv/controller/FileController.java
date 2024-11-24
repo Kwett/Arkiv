95dcda324fbc8c7ea8a9e0d0b509579688df219f
Kwett <coudrin.clement@gmail.com>
2024-11-24 12:57:13 +0100

    adds download mapping

--- Back/src/main/java/com/kwett/arkiv/controller/FileController.java
+++ Back/src/main/java/com/kwett/arkiv/controller/FileController.java
@@ -2,10 +2,16 @@ package com.kwett.arkiv.controller;
 
 import com.kwett.arkiv.model.FileInfo;
 import com.kwett.arkiv.repository.FileRepository;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -13,6 +19,7 @@ import java.util.List;
 public class FileController {
 
     private final FileRepository fileRepository;
+    private final Path fileStorageLocation = Paths.get("downloads").toAbsolutePath().normalize();
 
     public FileController(FileRepository fileRepository) {
         this.fileRepository = fileRepository;
@@ -21,5 +28,21 @@ public class FileController {
     @GetMapping
     public List<FileInfo> getFiles() {
         return fileRepository.findAll();
+    }
+
+
+
+    @GetMapping("/downloads/{fileName:.+}")
+    public ResponseEntity<UrlResource> downloadFile(@PathVariable String fileName) {
+        try {
+            Path filePath = fileStorageLocation.resolve(fileName).normalize();
+            UrlResource resource = new UrlResource(filePath.toUri());
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                    .body(resource);
+        } catch (Exception ex) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
